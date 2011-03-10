4335eadcce606b4726435988f6809b1f25dc5881
Scott Nelson <sbnelson@thermeon.com>
2011-03-10 01:15:11 +0000

    Added GPL info to beginning of what.c

--- what.c
+++ what.c
@@ -4,6 +4,21 @@
  * a non-printable character is found, or one of the following characters
  * are found: ", >, \, or new-line.
  *
+ *  Copyright (C) 2011 Scott Nelson
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <stdio.h>
@@ -12,7 +27,7 @@
 #include <string.h>
 
 static char *_filename;
-static char revision[]="@(#)$Header: /cvsroot/what/src/what.c,v 1.4 2011/03/10 01:02:57 sbnelson Exp $";
+static char revision[]="@(#)$Header: /cvsroot/what/src/what.c,v 1.5 2011/03/10 01:15:11 sbnelson Exp $";
 
 /* "wrapper" for standard fputs */
 int
